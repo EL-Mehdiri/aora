22803c8f205bcc82040d5c9929d5893158759cc3
Mehdi Ridmani <117091960+EL-Mehdiri@users.noreply.github.com>
2024-06-22 01:38:22 +0100

    update create and profile

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -8,6 +8,13 @@ interface Props {
   password: string;
 }
 
+interface VidoeProps {
+  title: String;
+  video: null;
+  thumbnail: null;
+  prompt: "";
+}
+
 // Register User
 export const createUser = async ({ username, email, password }: Props) => {
   try {
@@ -78,3 +85,27 @@ export async function getUserPosts(userId: string) {
     throw new Error(error);
   }
 }
+
+// createVideoPost
+export const createVideoPost = async ({
+  title,
+  video,
+  thumbnail,
+  prompt,
+}: VidoeProps) => {
+  try {
+    const response = await axios.post(`${API_URI}/auth/sign-up`, {
+      username,
+      email,
+      password,
+    });
+    const data = response.data;
+
+    if (data._id) {
+      console.log("user is created");
+    }
+    console.log("user not created!!");
+  } catch (error) {
+    console.log("error in appwrite");
+  }
+};
